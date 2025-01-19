0d30911629b77e566794b978c0a4578c2bd875f4
ERFouX <ERFouX@gmail.com>
2025-01-19 20:33:09 +0330

    create sign_up method

---
+++ config/sign_up.php
@@ -0,0 +1,33 @@
+<?php
+require_once('loader.php');
+if (isset($_POST['signup'])){
+    try{
+    // Parameters
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $mobile = $_POST['mobile'];
+    $password = $_POST['password'];
+
+    // SQL Query
+    $query = "INSERT INTO users SET username=?, email=?, mobile=?, password=?";
+
+    $stmt = $conn->prepare($query);
+
+    $stmt->bindValue(1, $username);
+    $stmt->bindValue(2, $email);
+    $stmt->bindValue(3, $mobile);
+    $stmt->bindValue(4, $password);
+
+    $stmt->execute();
+
+    header('Location: ../index.php');
+    }catch(PDOException $e){
+        echo "Your Error Massage Is : " . $e->getMessage();
+    }
+
+
+
+
+
+}
+?>
