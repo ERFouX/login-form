ca2bf1359f733ff18fe9f145119bf1fbe845d2e4
ERFouX <ERFouX@gmail.com>
2025-01-20 19:49:21 +0330

    create sign_in method

---
+++ config/sign_in.php
@@ -0,0 +1,31 @@
+<?php
+require_once('loader.php');
+if (isset($_POST['signin'])){
+    try {
+        // Parameters
+        $key = $_POST['key'];
+        $password = $_POST['password'];
+
+        // SQL Query
+        $query = "SELECT * FROM `users` WHERE (username = :key OR mobile = :key OR email = :key) AND (password = :password)";
+
+        $stmt = $conn->prepare($query);
+
+        $stmt->bindValue(":key", $key);
+        $stmt->bindValue(":password", $password);
+
+        $stmt->execute();
+
+        $hasUser = $stmt->rowCount();
+
+        if ($hasUser){
+            header ("Location: ./login_result.php?loginned=yes");
+        }else {
+            header("Location: ./login_result.php?usernotfound=yes");
+        }
+
+    } catch (PDOException $e) {
+        echo "Your Error Message Is: " . $e->getMessage();
+    }
+}
+?>
