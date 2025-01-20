c5cc7aa69821ff97a0cb4dab1ed13c21f88d627b
ERFouX <ERFouX@gmail.com>
2025-01-20 19:43:26 +0330

    fix sign in form problems and add bootstrap

--- index.php
+++ index.php
@@ -1,12 +1,16 @@
 <?php
 require_once('config/loader.php');
-
 ?>
 
-<html lang="pt-BR">
+<html lang="EN">
     <head>
         <meta charset="UTF-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <link
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+            rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
+            crossorigin="anonymous"
+        />
         <link
             rel="stylesheet"
             href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
@@ -39,11 +43,11 @@ require_once('config/loader.php');
                 <form method="POST" action="config/sign_in.php">
                     <h1 class="mb-30">Sign In</h1>
                     <span>or use your email/password</span>
-                    <input type="email" placeholder="Email / Username / Mobile" />
-                    <input type="password" placeholder="Password" />
+                    <input type="text" name="key" placeholder="Email / Username / Mobile" required />
+                    <input type="password" name="password" placeholder="Password" required />
                     <a href="#">Forget your Password?</a>
                     <div style="display: inline;">
-                        <button>Sign In</button>
+                        <button type="submit" name="signin">Sign In</button>
                         <a style="margin-left: 15px; color: #512da8;" href="otp.php">Send OTP</a>
                     </div>
                 </form>
