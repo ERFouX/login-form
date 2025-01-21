8acd36ac77f31d791b8c7c92825e0b158c01f98c
ERFouX <ERFouX@gmail.com>
2025-01-21 23:23:24 +0330

    Refactor HTML structure and improve user feedback messages on login result pages

--- config/login_result.php
+++ config/login_result.php
@@ -1,4 +1,4 @@
-    
+<html lang="EN">
     <head>
         <meta charset="UTF-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
@@ -6,24 +6,43 @@
             href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
             rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
             crossorigin="anonymous"
-        /> 
+        />
+        <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
+            integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
+            crossorigin="anonymous"
+            referrerpolicy="no-referrer"
+        />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Login Result</title>
+        <link rel="stylesheet" href="../assets/css/style.css" />
     </head>
-    <div class="container mt-5">
-        <?php if(isset($_GET['usernotfound'])){?>
-            <div class="alert alert-danger">
-                <p>Invalid username or password.</p>
-            </div>
-        <?php }else if(isset($_GET['loginned'])){?>
-            <div class="alert alert-success">
-                <p>You are Loginned Successfully!</p>
-            </div>
-        <?php }else if(isset($_GET['account_created'])){?>
-            <div class="alert alert-info">
-                <p>Your Account Created Successfully</p>
-            </div>
-        <?php }?>
 
-        <a href="../index.php" class="btn btn-info">Back To Login Form</a>
-    </div>
+    <body>
+        <div class="container" id="container">
+            <div class="otp-container">
+                <form>
+                    <?php if(isset($_GET['usernotfound'])){?>
+                        <div class="alert alert-danger">
+                            <h1>Invalid username or password.</h1>
+                        </div>
+                    <?php }else if(isset($_GET['loginned'])){?>
+                        <div class="alert alert-success">
+                            <h1>You are Loginned Successfully!</h1>
+                        </div>
+                    <?php }else if(isset($_GET['account_created'])){?>
+                        <div class="alert alert-info">
+                            <h1>Your Account Created Successfully</h1>
+                        </div>
+                    <?php }?>
+
+                    <button>
+                        <a href="../index.php" style="color:white;">Back To Login Form</a>
+                    </button>
+                </form>
+            </div>
+        </div>
+    </body>
+    <script src="./assets/scripts/script.js"></script>
+</html>
