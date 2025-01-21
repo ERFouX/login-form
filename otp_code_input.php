da44cb671d9876aca898e24284402cbf0dbd0e3d
ERFouX <ERFouX@gmail.com>
2025-01-21 21:53:26 +0330

    Create a form to enter the otp code

---
+++ otp_code_input.php
@@ -0,0 +1,24 @@
+<html lang="EN">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Enter Otp Code</title>
+        <link rel="stylesheet" href="./assets/css/style.css">
+    </head>
+    <body>
+        <div class="container" id="container">
+            <div class="otp-container">
+                <form method="POST">
+                    <h1 class="mb-25"">Enter Your OTP Code</h1>
+                    <span class="mb-25">We have sent you a 5-digit code, enter it in the field below</span>
+                    <input type="number" placeholder="Enter Otp Code" style="max-width: 350px;">
+                    <button type="submit" name="check_otp">Submit</button>
+                    <a href="otp.php">Back To OTP Form</a>
+                </form>
+            </div>
+        </div>
+    </body>
+    <script src="./assets/scripts/script.js"></script>
+</html>
