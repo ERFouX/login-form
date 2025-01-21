a2b45f06e0925f602ced00c3a83f2ce9083965bb
ERFouX <ERFouX@gmail.com>
2025-01-22 00:07:34 +0330

    update sign_up.php

--- config/sign_up.php
+++ config/sign_up.php
@@ -2,33 +2,28 @@
 require_once('database.php');
 if (isset($_POST['signup'])){
     try{
-    // Parameters
-    $username = $_POST['username'];
-    $email = $_POST['email'];
-    $mobile = $_POST['mobile'];
-    $password = $_POST['password'];
+        // Parameters
+        $username = $_POST['username'];
+        $email = $_POST['email'];
+        $mobile = $_POST['mobile'];
+        $password = $_POST['password'];
 
-    // SQL Query
-    $query = "INSERT INTO users SET username=?, email=?, mobile=?, password=?";
+        // SQL Query
+        $query = "INSERT INTO users SET username=?, email=?, mobile=?, password=?";
 
-    $stmt = $conn->prepare($query);
+        $stmt = $conn->prepare($query);
 
-    $stmt->bindValue(1, $username);
-    $stmt->bindValue(2, $email);
-    $stmt->bindValue(3, $mobile);
-    $stmt->bindValue(4, $password);
+        $stmt->bindValue(1, $username);
+        $stmt->bindValue(2, $email);
+        $stmt->bindValue(3, $mobile);
+        $stmt->bindValue(4, $password);
 
-    $stmt->execute();
+        $stmt->execute();
 
-    header('Location: ./login_result.php?account_created=true');
+        header('Location: ./login_result.php?account_created=true');
 
-    }catch(PDOException $e){
+    } catch(PDOException $e){
         echo "Your Error Massage Is : " . $e->getMessage();
     }
-
-
-
-
-
 }
 ?>
